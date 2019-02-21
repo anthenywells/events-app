e4b2199d245e38f9d583cc20fd76d1d8eb83de68
Antheny Wells <anthenywells@Go-away.lan>
2019-02-20 20:59:00 -0500

    added hover states

--- app/bucketlistPage/bucketlistPage.component.js
+++ app/bucketlistPage/bucketlistPage.component.js
@@ -3,6 +3,7 @@ const bucketList = {
     templateUrl: "app/bucketListPage/bucketListPage.html",
     controller: ["EventService", function(EventService) {
         const vm = this
+        vm.toggle = true;
         vm.removeFavorite = (index) => {
             EventService.deleteFavorite(index)
           }
